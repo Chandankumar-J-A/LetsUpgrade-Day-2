3cd6b0860f6a6c2dd15380830048303545b10db0
Chandankumar J A <57993659+Chandankumar-J-A@users.noreply.github.com>
2020-07-15 20:42:46 +0530

    Add files via upload

---
+++ Question-1.js
@@ -0,0 +1,2 @@
+let Name = prompt("Enter your name please..!");
+console.log("Your name is "+ Name);
---
+++ Question-3.js
@@ -0,0 +1,8 @@
+let Age = prompt("Enter your Age please..!");
+
+console.log("Your Age is "+ Age);
+if (Age > 21) {
+    console.log("You can drink...! ");
+  } else {
+    console.log("You can't drink...! ");
+  }
